ae873209edf6d522e565cd8beb3f2f7876a465f6
Bruno Ferrari <b.ferrari09@gmail.com>
2024-05-26 22:21:44 -0300

    Cleanup imports

--- app/src/main/java/com/ferrarib/todolist/presentation/details/DetailsScreen.kt
+++ app/src/main/java/com/ferrarib/todolist/presentation/details/DetailsScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.ferrarib.todolist.R
 import com.ferrarib.todolist.ui.components.ScreenTitle
 import com.ferrarib.todolist.ui.tokens.SizeTokens
