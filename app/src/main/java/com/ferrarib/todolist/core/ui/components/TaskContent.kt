a3336442cd8b5fca4111a7b7f3e0033f2b331eb0
Bruno Ferrari <b.ferrari09@gmail.com>
2024-05-27 20:47:57 -0300

    Change remember to rememberSaveable

--- app/src/main/java/com/ferrarib/todolist/core/ui/components/TaskContent.kt
+++ app/src/main/java/com/ferrarib/todolist/core/ui/components/TaskContent.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -24,7 +25,7 @@ fun TaskContent(
     task: Task?,
     onSaveButtonPressed: (String) -> Unit
 ) {
-    var content by remember { mutableStateOf("") }
+    var content by rememberSaveable { mutableStateOf("") }
     val addingNew = task == null
 
     TextField(
